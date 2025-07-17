edcb92b9fc121780151ac2d3eb9aba36ab6582c6
Lilith Lindsey <lilithx1313@gmail.com>
2025-07-17 21:40:23 +0000

    added necessary code to generate a random excuse and cleaned up formatting

--- src/app.js
+++ src/app.js
@@ -5,7 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+function generateExcuse() {
+  let who = ["My Grandma", "The dog", "My friend", "The mailman"];
+  let action = ["ate", "peed on", "burned", "lost"];
+  let what = ["my homework", "my car", "the principal", "my backpack"];
+  let when = ["before class.", "this morning.", "during my workout.", "last night."];
+
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+
+  return randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen;
+}
+
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+    document.getElementById('excuse').innerHTML = generateExcuse();
 };
